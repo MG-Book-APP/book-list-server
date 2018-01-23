54c24824dd8b37eb61fb616f1c4705ba2f7a45de
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-01-23 14:02:22 -0800

    console.log loadBooks

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ app.get('/', (req, res) => res.send('hello world'));
 loadDB();
 
 function loadBooks() {
-  fs.readFile('./book-list-client/data/books.json', function(err, fd) {
+  fs.readFile('../book-list-client/data/books.json', function(err, fd) {
+    console.log(err);
     JSON.parse(fd.toString()).forEach(function(ele) {
       client.query(
         'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
