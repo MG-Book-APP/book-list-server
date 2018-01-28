340ae3542dac018425d9c400b3b7f700c8bce5ed
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-28 15:26:06 -0800

    server comment

--- server.js
+++ server.js
@@ -8,19 +8,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT;
 
-// const connectionString = 'postgres://localhost:5432/books';
-// const connectionString = 'postgres://xealeyifxbxtdh:e1af3e1ffdc99abe7bb7d3dddfe719c695d9da36fb1aa8c78c92daa52f9e89a1@ec2-23-21-195-249.compute-1.amazonaws.com:5432/deptsmaqkmiugb';
 const connectionString = process.env.DATABASE_URL;
 const client = new pg.Client(connectionString);
 client.connect();
 
 app.use(cors());
-app.use(function(req, res, next) {
-  req.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -77,6 +69,7 @@ app.delete('/api/v1/books/:id', function(req,res) {
     })
 });
 
+// Doesn't work as of Sunday, January 28.
 app.put('/api/v1/books/:id/:edit', function(req,res) {
   console.log('Server',req.body)
   client.query(`UPDATE * FROM WHERE id = ${req.params.id};`)
@@ -90,14 +83,14 @@ app.put('/api/v1/books/:id/:edit', function(req,res) {
           description=$5
       WHERE id = $6;
       `,
-      [
-        req.body.title,
-        req.body.author,
-        req.body.isbn,
-        req.body.image_url,
-        req.body.description,
-        req.params.id
-      ]
+        [
+          req.body.title,
+          req.body.author,
+          req.body.isbn,
+          req.body.image_url,
+          req.body.description,
+          req.params.id
+        ]
       )
     })
     .then(() => res.send('Book has been updated.'))
